ddbd2ef2cb1ee95e2bf0c7efff27fbd0f325838b
Blake Arnold <blake.arnold@mail.com>
2025-05-28 13:39:46 -0700

    Update School Anti-Block.bat

--- School Anti-Block.bat
+++ School Anti-Block.bat
@@ -1,98 +1,31 @@
 @echo off
-setlocal disableDelayedExpansion
-set q=^"
-Color 07
-Title School Anti-Block
-Echo.
-call :c 0D "================================" /n
-call :c 0C "    Windows School Anti-Block" /n
-call :c 09 "          Created by:" /n
-call :c 0B "          @yodaluca23" /n
-call :c 0A "            v1.3.1" /n
-call :c 0D "================================" /n
-Echo.
-Echo.
-Echo.
-pause
-:Error
-cls
+setlocal enabledelayedexpansion
+set "GUIexe=GUI.exe"
+
+:: Start GUI process picker and wait for it to close
+if exist "%GUIexe%" (
+    start /wait "" "%GUIexe%"
+) else (
+    echo %GUIexe% not found. Exiting.
+    pause
+    exit /b
+)
+
+:: Read processes.txt to get process list
+if not exist processes.txt (
+    echo processes.txt not found. Exiting.
+    pause
+    exit /b
+)
+
+set "PLIST="
+for /f "usebackq delims=" %%P in ("processes.txt") do (
+    set "PLIST=!PLIST! %%P"
+)
+
 :Task
-taskkill /F /im lsproxy.exe
-taskkill /F /im client32.exe
-taskkill /F /im Client32.exe
-taskkill /F /im Runplugin64.exe
-taskkill /F /im runplugin.exe
-
-//ClassPolicy
-net stop "classpolicyagent"
-
-taskkill /F /im LSSASvc.exe
-Echo Now attempting to kill processes...
-taskkill /F /im smart-agent-js-win.exe
-taskkill /F /im makeca.exe
+for %%P in (%PLIST%) do (
+    taskkill /F /im %%P
+)
+timeout /t 2 >nul
 goto Task
-Echo.
-Echo.
-Echo An Error Has Ocurred.
-Echo.
-Echo Would you like to retry?
-pause
-goto Error
-Echo.
-Echo.
-Echo An Unforseen Error Has Occured. Please Reopen The Script.
-Echo.
-:c
-setlocal enableDelayedExpansion
-:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-
-:colorPrint Color  Str  [/n]
-setlocal
-set "s=%~2"
-call :colorPrintVar %1 s %3
-exit /b
-
-:colorPrintVar  Color  StrVar  [/n]
-if not defined DEL call :initColorPrint
-setlocal enableDelayedExpansion
-pushd .
-':
-cd \
-set "s=!%~2!"
-for %%n in (^"^
-
-^") do (
-  set "s=!s:\=%%~n\%%~n!"
-  set "s=!s:/=%%~n/%%~n!"
-  set "s=!s::=%%~n:%%~n!"
-)
-for /f delims^=^ eol^= %%s in ("!s!") do (
-  if "!" equ "" setlocal disableDelayedExpansion
-  if %%s==\ (
-    findstr /a:%~1 "." "\'" nul
-    <nul set /p "=%DEL%%DEL%%DEL%"
-  ) else if %%s==/ (
-    findstr /a:%~1 "." "/.\'" nul
-    <nul set /p "=%DEL%%DEL%%DEL%%DEL%%DEL%"
-  ) else (
-    >colorPrint.txt (echo %%s\..\')
-    findstr /a:%~1 /f:colorPrint.txt "."
-    <nul set /p "=%DEL%%DEL%%DEL%%DEL%%DEL%%DEL%%DEL%"
-  )
-)
-if /i "%~3"=="/n" echo(
-popd
-exit /b
-
-
-:initColorPrint
-for /f %%A in ('"prompt $H&for %%B in (1) do rem"') do set "DEL=%%A %%A"
-<nul >"%temp%\'" set /p "=."
-subst ': "%temp%" >nul
-exit /b
-
-
-:cleanupColorPrint
-2>nul del "%temp%\'"
-2>nul del "%temp%\colorPrint.txt"
->nul subst ': /d
